d82f93f09d7865c224ed3bb20847b753211a6807
Manishathonti <manishathontii@gmail.com>
2025-02-18 17:19:59 -0500

    Update SQL_PROJECT_1.sql

--- SQL_PROJECT_1.sql
+++ SQL_PROJECT_1.sql
@@ -1,3 +1,20 @@
+--Create Table--
+
+CREATE TABLE retail_sales
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,	
+    sale_time TIME,
+    customer_id INT,	
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,	
+    cogs FLOAT,
+    total_sale FLOAT
+);
+
 --Data Cleaning--
 
 DELETE from retail_sales
